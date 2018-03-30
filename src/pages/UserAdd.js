14068605bb7ef7e8623c71e9ab4840d0236eb314
hesisi <hesisi2016@foxmail.com>
2018-03-30 15:10:26 +0800

    all codes

--- src/pages/UserAdd.js
+++ src/pages/UserAdd.js
@@ -1,98 +1,44 @@
 import React from 'react';
+import formProvider from '../utils/formProvider';
 
 class UserAdd extends React.Component{
-    constructor(){
-        super();
-        this.state = {
-            form : {
-                name : {
-                    valid : false,
-                    value : "",
-                    error : ""
-                },
-                age : {
-                    valid : false,
-                    value : 0,
-                    error : ""
-                },
-                gender : {
-                    valid : false,
-                    value : "",
-                    error : ""
-                }
-            }
-        };
-    }
-
-    handleValueChange(field,value,type="string"){
-        if(type === "number"){
-            value =+ value;
-        }
-
-        const {form} = this.state;
-
-        const newFieldObj = {valid:true,value,error:''};
-
-        switch(field){
-            case 'name' : {
-                if(calue.length >= 5){
-                    newFieldObj.error = "用户名最多4个字符";
-                    newFeildObj.valid = false;
-                }else if(value.length === 0){
-                    newFieldObj.error = "请输入用户名";
-                    newFieldObj.valid = false;
-                }
-                break;
-            }
-            case 'age' : {
-                if(value > 150 || value <= 0){
-                    newFieldObj.error = "请传入"
-                }
-            }
-        }
-
-        this.setState({
-            [field] : value
-        });
-    }
-
+    //点击提交
     handleSubmit(e){
         e.preventDefault();
-        //添加一条数据
-        const {name, age, gender} = this.state;
 
-        fetch("http://localhost:3001/user/",{
-            method : "post",
+        const {form:{name,age,gender}, formValid} = this.props;  //formProvider返回的组件传过来的props
+        if(!formValid){
+            alert('请填写正确信息后重试!');
+            return;
+        }
+
+        //请求
+        fetch('http://localhost:3001/user',{
+            method : 'post',
             body : JSON.stringify({
-                name,
-                age,
-                gender
+                name : name.value,
+                age : age.value,
+                gender : gender.value
             }),
             headers : {
-                'Content-Type': 'application/json'
+                'Content-Type':'application/json'
             }
         })
         .then((res) => res.json())
         .then((res) => {
             if(res.id){
-                alert("添加成功！");
-                this.setState({  //重置state
-                    name : "",
-                    age : 0,
-                    gender : ""
-                });
+                alert("添加成功!");
             }else{
-                alert(JSON.stringify(res)+"添加失败!");
+                alert("添加失败！");
             }
-        })
-        .catch((error) => {
-            console.error(error);
+        }).catch((err) => {
+            console.error(err);
         });
-
     }
 
+    //render返回的组件格式
     render(){
-        const {name,age,gender} = this.state;
+        const { form :{name ,age ,gender} ,onFormChange} = this.props;
         return (
             <div>
                 <header>
@@ -101,25 +47,90 @@ class UserAdd extends React.Component{
 
                 <main>
                     <form onSubmit={(e) => this.handleSubmit(e)}>
-                        <label>namme:</label>
-                        <input type="text" value={name} onChange={(e) => this.handleValueChange('name',e.target.value)}/>
+                        <label>用户名：</label>
+                        <input 
+                            type="text"
+                            value={name.value}
+                            onChange={(e) => onFormChange("name",e.target.value)}
+                        />
+                        {!name.valid && <span>{name.error}</span>}
                         <br/>
-                        <label>age:</label>
-                        <input type="number" value={age || ''} onChange={(e) => this.handleValueChange('age',e.target.value)}/>
+
+                        <label>年龄：</label>
+                        <input
+                            type="number"
+                            value={age.value || ""}
+                            onChange={(e) => onFormChange('age',e.target.value)}
+                        />
+                        {!age.valid && <span>{age.error}</span>}
                         <br/>
-                        <label>gender:</label>
-                        <select value={gender} onChange={(e) => this.handleValueChange('gender',e.target.value)}> 
-                            <option value="">choises</option>
-                            <option value="male">male</option>
-                            <option value="female">female</option>
+
+                        <label>性别</label>
+                        <select
+                            value={gender.value}
+                            onChange={(e) => onFormChange('gender',e.target.value)}
+                        >
+                            <option value="">请选择</option>
+                            <option value="male">男</option>
+                            <option value="female">女</option>
                         </select>
+                        {!gender.valid && <span>{gender.error}</span>}
                         <br/>
-                        <input type="submit" value="submit"/>
+                        <br/>
+
+                        <input type="submit" value="提交"/>
                     </form>
                 </main>
             </div>
         );
     }
+
 }
 
+
+//formProvider
+UserAdd = formProvider({
+    name : {
+        defaultValue : '',
+        rules : [
+            {
+                pattern : function(value){
+                    return value.length > 0;
+                },
+                error : '请输入姓名'
+            },{
+                pattern : /^.{1,6}$/,
+                error : '用户名不能超过6个字'
+            }
+        ]
+    },
+    age : {
+        defaultValue : 0,
+        rules : [
+            {
+                pattern : function(value){
+                    return value.length > 0;
+                },
+                error : '请输入年龄'
+            },
+            {
+                pattern : function(value){
+                    return value >=1 && value <= 150;
+                },
+                error : '请输入1~150之间的数字'
+            }
+        ]
+    },
+    gender : {
+        defaultValue : "",
+        rules : [
+            {
+                pattern : function(value){
+                    return !!value;
+                },
+                error:'请选择性别'
+            }
+        ]
+    }
+})(UserAdd);
 export default UserAdd;
---
+++ src/utils/formProvider.js
@@ -0,0 +1,156 @@
+import React from 'react';
+
+/**
+fields是一个对象,格式如下
+{
+    name : {
+        defaultValue : '',
+        rules : [
+            {
+                pattern : function(value){
+                    return value.length > 0;
+                },
+                error : '请输入姓名'
+            },{
+                pattern : /^.{1,6}$/,
+                error : '用户名不能超过6个字'
+            }
+        ]
+    },
+    age : {
+        defaultValue : 0,
+        rules : [
+            {
+                pattern : function(value){
+                    return value.length > 0;
+                },
+                error : '请输入年龄'
+            },
+            {
+                pattern : function(value){
+                    return value >=1 && value <= 150;
+                },
+                error : '请输入1~150之间的数字'
+            }
+        ]
+    },
+    gender : {
+        defaultValue : "",
+        rules : [
+            {
+                pattern : function(value){
+                    return !!value;
+                },
+                error:'请选择性别'
+            }
+        ]
+    }
+}} 
+ */
+
+
+//initialFormState格式
+/*
+{
+    param : {
+        value : 'value',
+        error : ''
+    },
+    param : {
+        value : 'value',
+        error : ''
+    },
+    param : {
+        value : 'value',
+        error : ''
+    }
+}
+*/
+
+//sate格式
+/*
+{
+    form : {
+        param : {
+            value : 'value',
+            error : ''
+        },
+        param : {
+            value : 'value',
+            error : ''
+        },
+        param : {
+            value : 'value',
+            error : ''
+        }
+    },
+    formValid : false
+}
+*/
+function formProvider(fields){  //fields是一个对象
+    return function(Comp){
+        const initialFormState = {};
+        for(const key in fields){
+            initialFormState[key] = {
+                value : fields[key].defaultValue,
+                error : ''
+            };
+        }
+
+        class FormComponent extends React.Component{
+            constructor(props){
+                super(props);
+                this.state = {
+                    form : initialFormState,
+                    formValid : false
+                };
+
+                this.handleValueChange = this.handleValueChange.bind(this);
+            }
+
+            handleValueChange (fieldName,value){
+                const { form } = this.state; //把初始化的form赋值给该变量
+
+                const newFieldState = {value, valid:true, error:""};
+
+                const fieldRules = fields[fieldName].rules;  //某个字段对应的校验规则[{pattern1:"",error1:""},{pattern2:"",error2:""}...]
+
+                for(let i = 0;i<fieldRules.length;i++){
+                    const {pattern,error} = fieldRules[i];  //校验规则对应的格式{pattern:"",error:""}
+                    let valid = false;
+                    if(typeof pattern === "function"){
+                        valid = pattern(value);
+                    }else{
+                        valid = pattern.test(value);
+                    }
+
+                    if(!valid){
+                        newFieldState.valid = false;
+                        newFieldState.error = error;
+                        break;
+                    }
+                }
+
+                const newForm = {...form,[fieldName]:newFieldState};  //信息合法则修改form的value值
+                const formValid = Object.values(newForm).every(f => f.valid);  
+                
+                //Object返回对象所有枚举类的属性值数组
+                //every遍历数组并去除数组中所有对象的valid的值
+
+                this.setState({
+                    form: newForm,
+                    formValid
+                });
+            }
+
+            render(){
+                const {form,formValid} = this.state;
+                //将form,formValid,onFormChange三个props传给组件并返回
+                return <Comp {...this.props} form={form} formValid={formValid} onFormChange={this.handleValueChange}/>;
+            }
+        }
+        return FormComponent;
+    };
+}
+
+export default formProvider;
